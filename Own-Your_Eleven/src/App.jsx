a2feaa2b37b7c528b04d88bd7fdfce76ae58623d
MD Arafat Alam Leom <mdarafatalamleon10@gmail.com>
2026-02-14 22:47:02 +0600

    added dynamicBtn

--- Own-Your_Eleven/src/App.jsx
+++ Own-Your_Eleven/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 import Navbar from './components/Navbar/Navbar.jsx'
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers.jsx'
-// import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers.jsx'
+ import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers.jsx'
 
 const fatchPlayers = async () => {
   const res =  await fetch('./players.json')
@@ -15,21 +15,27 @@ const fatchPlayers = async () => {
 
 function App() {
 
+const [toggle,setToggle]=useState(false)
   const PlayersPromise= fatchPlayers()
 
   return (
     <>
 
 <Navbar></Navbar>
-<div className='border-2 border-red-500 max-w-[1200px] mx-auto flex items-center justify-between mt-10'>
- <h1>Available Players</h1>
+<div className='max-w-[1200px] mx-auto flex items-center justify-between mt-10'>
+ <h1 className='font-bold text-2xl'>Available Players</h1>
  <div>
-  <button className='p-3 border-1 border-blue-700 rounded-l-2xl'>Available</button>
-  <button  className='p-3 border-1 border-blue-700 rounded-r-2xl'>Selected <span>(0)</span></button>
+  <button onClick={()=>setToggle(true)
+  } className={`p-3 border-1 border-blue-700 rounded-l-2xl border-r-0 ${toggle === true ? 'bg-[#E7FE29]' : ''} text-black font-bold`}>Available</button>
+  <button onClick={()=>{setToggle(false
+  )}} className={`p-3 border-1 border-blue-700 rounded-r-2xl border-l-0 font-bold text-black ${toggle === false ? 'bg-[#E7FE29]' : ''}`}>Selected <span>(0)</span></button>
  </div>
 </div>
-<Suspense fallback={<span className="loading loading-spinner text-error"></span>}><AvailablePlayers PlayersPromise={PlayersPromise}></AvailablePlayers></Suspense>
-{/* <SelectedPlayers></SelectedPlayers> */}
+
+{
+  toggle === true ? <Suspense fallback={<span className="loading loading-spinner text-error"></span>}><AvailablePlayers PlayersPromise={PlayersPromise}></AvailablePlayers></Suspense>: <SelectedPlayers></SelectedPlayers>
+}
+
 
 
     </>
---
+++ Own-Your_Eleven/src/components/PlayerCard/PlayerCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import humanImg from '../../assets/Group.png'
+import reportImg from '../../assets/report.png'
+
+const PlayerCard = ({playersData}) => {
+    return (
+        <div className="card bg-base-100  shadow-sky-900 bg-zinc-600" >
+  <figure>
+    <img className='w-full h-[300px] object-cover'
+      src={playersData.img} alt="Player" />
+  </figure>
+  <div className="mt-4">
+<div  className='flex'>
+     <img src={humanImg} alt="" />
+    <h2 className="card-title ml-2">{playersData.name}</h2>
+</div>
+  
+<div  className='flex items-center justify-between border-b-2 border-gray-400 pb-2'>
+
+<div  className='flex items-center mt-4 '>
+    <img className="border-2 " src={reportImg} alt="" />
+    <span className='ml-1'>{playersData.country}</span>
+</div>
+
+ <button className='btn'>All rounder</button>
+</div>
+<div className='flex justify-between font-bold'>
+  <span>Rating</span>
+  <span>5</span>
+</div>
+
+<div className='flex justify-between font-bold'>
+  <span className='font-bold'>{playersData.role}</span>
+  <span>{playersData.battingStyle}</span>
+</div>
+
+    <div className="card-actions justify-between mt-4">
+      <span className='font-bold'>Price: ${playersData.price}</span>
+      <button className="btn btn-primary">Choose Player</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default PlayerCard;
